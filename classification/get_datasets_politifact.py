cefb607b9e72be0f4ffe959cc752885197da0bef
Vlad Dumitru <d.vladk@gmail.com>
2021-01-04 20:09:40 +0200

    2-class classification

---
+++ classification/get_datasets_politifact.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+files = ["politifact.csv", "politifact_covid.csv"]
+folder = "PolitifactDatasets/"
+
+def write_csv(filename, col_names, cols):
+    df = pd.DataFrame(cols)
+    df = df.transpose()
+
+    with open(filename, 'w', encoding='utf-8', newline = '\n') as f:
+        df.to_csv(f, header=col_names)
+
+def get_train_test_files():
+    for file in files:
+        train_df_list = []
+        test_df_list = []
+        df = pd.read_csv(file)
+        df_label_0 = df[df['label'] == 0]
+        df_label_1 = df[df['label'] == 1]
+        df_label_2 = df[df['label'] == 2]
+        df_label_3 = df[df['label'] == 3]
+        df_list = [df_label_0, df_label_1, df_label_2, df_label_3]
+        for item in df_list:
+            train_df, test_df = train_test_split(item, test_size = 0.25)
+            train_df_list.append(train_df)
+            test_df_list.append(test_df)
+
+        train_ = pd.concat(train_df_list)
+        test_ = pd.concat(test_df_list)
+
+        train_filename = folder + file.split('.')[0] + "_train.csv"
+        test_filename = folder + file.split('.')[0] + "_test.csv"
+
+        print (file)
+        for label in range(4):
+            train = train_[train_['label'] == label].shape[0]
+            test = test_[test_['label'] == label].shape[0]
+            total_label = train + test
+            print ("Label ", label, ": -> train: ", train,
+                "(", train * 100 / total_label,"%) ",
+                "| -> test: ", test,
+                "(", test * 100 / total_label,"%) ",
+                "| total: ", total_label)
+
+        write_csv(train_filename, train_.columns.values, [train_[column].to_list() for column in list(train_.columns.values)])
+        write_csv(test_filename, test_.columns.values, [test_[column].to_list() for column in list(test_.columns.values)])
+
+def main():
+    get_train_test_files()
+
+if __name__ == "__main__":
+    main()
---
+++ classification/model_politifact.py
@@ -0,0 +1,121 @@
+import argparse
+
+import pandas as pd
+from simpletransformers.classification import ClassificationModel
+
+folder = "PolitifactDatasets/"
+train_files = [folder + "politifact_train.csv", folder + "politifact_covid_train.csv"]
+test_files = [folder + "politifact_test.csv", folder + "politifact_covid_test.csv"]
+
+import torch.multiprocessing
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+class Model:
+    def __init__(self):
+        self.train_args = {
+            "reprocess_input_data": True,
+            "fp16": False,
+            "num_train_epochs": 8,
+        }
+
+    def generate_df(self, input_file):
+        df = pd.read_csv(input_file)
+        df["labels"] = df["label"]
+        return df[["text", "labels"]]
+
+    def read_df(self, input_file):
+        df = pd.read_csv(input_file)
+        df["labels"] = df["label"]
+        df = df.drop(['Unnamed: 0'], axis=1)
+        df = df.drop(['label'], axis=1)
+        return df
+
+    def train_model(self, train_file, model_type, model_name, out_dir=None, use_cuda=False):
+        self.train_args["output_dir"] = out_dir
+        train_df = self.generate_df(train_file)
+        model = ClassificationModel(
+            model_type,
+            model_name,
+            num_labels=2,
+            args=self.train_args,
+            use_cuda=use_cuda,
+        )
+
+        model.train_model(
+            train_df,
+            output_dir=out_dir,
+        )
+
+    def test_model(self, test_file, model_type, model_dir, use_cuda=False):
+        self.train_args["output_dir"] = out_dir
+        test_df = self.generate_df(test_file)
+        model = ClassificationModel(
+            model_type,
+            f"{model_dir}/",
+            num_labels=2,
+            args=self.train_args,
+            use_cuda=use_cuda,
+        )
+        result, model_outputs, wrong_predictions = model.eval_model(
+            test_df
+        )
+        print(result)
+
+    def predict(self, test_file, model_type, model_dir, use_cuda=False):
+        self.train_args["output_dir"] = out_dir
+        test_df = self.read_df(test_file)
+        model = ClassificationModel(
+            model_type,
+            f"{model_dir}/",
+            num_labels=4,
+            args=self.train_args,
+            use_cuda=use_cuda,
+        )
+        predictions, raw_outputs = model.predict(test_df['text'].to_list())
+        test_df['predictions'] = predictions
+        test_predictions = f"{test_file[:-4]}_predictions_{model_type}.csv"
+        write_csv(test_predictions, list(test_df.columns.values), [test_df[column].to_list() for column in list(test_df.columns.values)])
+
+def write_csv(filename, col_names, cols):
+    df = pd.DataFrame(cols)
+    df = df.transpose()
+
+    with open(filename, 'w', encoding='utf-8', newline = '\n') as f:
+            df.to_csv(f, header=col_names)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--train", action="store_true", help="train model")
+    parser.add_argument("-e", "--eval", action="store_true", help="eval model")
+    parser.add_argument("-p", "--predict", action="store_true", help="eval model")
+    parser.add_argument("--use-cuda", action="store_true", help="Use CUDA for the model")
+    parser.add_argument("--model-type", type=str, default="bert")
+    parser.add_argument("--model-name", type=str, default="bert-base-cased")
+
+    args = parser.parse_args()
+
+    if not args.train and not args.eval and not args.predict:
+        parser.print_help()
+        exit(0)
+
+    model_type = args.model_type
+    model_name = args.model_name
+
+    model = Model()
+    if args.train:
+        for train_file in train_files:
+            print(f"Training {model_type}: {model_name} on {train_file}")
+            out_dir = f"{train_file.split('.')[0]}_{model_name}"
+            model.train_model(train_file, model_type, model_name, out_dir, args.use_cuda)
+
+    if args.eval:
+        for i, train_file in enumerate(train_files):
+            print(f"Evaluating {model_type}: {model_name} on {test_files[i]}")
+            out_dir = f"{train_file.split('.')[0]}_{model_name}"
+            model.test_model(test_files[i], model_type, out_dir, args.use_cuda)
+
+    if args.predict:
+        for i, train_file in enumerate(train_files):
+            print(f"Predicting {model_type}: {model_name} on {test_files[i]}")
+            out_dir = f"{train_file.split('.')[0]}_{model_name}"
+            model.predict(test_files[i], model_type, out_dir, args.use_cuda)
