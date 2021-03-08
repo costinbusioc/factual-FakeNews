69b78031f4282a1d581653700efdef0c0600641b
Vlad Dumitru <d.vladk@gmail.com>
2021-03-08 18:05:23 +0200

    Update

--- classification/regression.py
+++ classification/regression.py
@@ -118,6 +118,9 @@ def main():
 		with open(model_name, 'rb') as file:
 			reg = pickle.load(file)
 		predictions = reg.predict(features)
+		for index in range(len(predictions)):
+			print ('Predictie: ', predictions[index])
+			print ('Real: ', labels[index])
 		score = mean_squared_error(labels, predictions)
 		print ("Mean squarred error test: ", score)
 
