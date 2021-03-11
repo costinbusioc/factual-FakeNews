80fff9cec8b5a867d6e60b61b8fb245fa5289284
Vlad Dumitru <d.vladk@gmail.com>
2021-03-11 19:09:06 +0200

    Add epochs and lr

--- classification/regression_rb.py
+++ classification/regression_rb.py
@@ -69,7 +69,8 @@ def run_bert_rb(
             if isinstance(layer, bert.BertModelLayer):
                 layer.trainable = False
 
-    model.compile(loss='mean_squared_error')
+    optimizer = keras.optimizers.Adam(lr=1e-5)
+	model.compile(loss='mean_squared_error', optimizer=optimizer)
     bert_wrapper.load_weights()
 
     articles_train = statements_to_list(statements_train, first_validation_pars_train, last_validation_pars_train)
@@ -78,7 +79,7 @@ def run_bert_rb(
     feed_inputs_train = bert_wrapper.process_input(articles_train)
     feed_inputs_test = bert_wrapper.process_input(articles_test)
 
-    model.fit(feed_inputs_train, np.asarray(labels_train))
+    model.fit(feed_inputs_train, np.asarray(labels_train), epochs=10)
 
     # result = model.predict(feed_inputs_test, batch_size=32)
     print(f"Mean squared error train: {model.evaluate(feed_inputs_train, np.asarray(labels_train))}")
