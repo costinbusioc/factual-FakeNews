a33f307973780d03887878068b24733c748f290d
Costin Busioc <busioccostin@gmail.com>
2021-05-18 22:06:06 +0300

    Update

--- es_analysis/helpers.py
+++ es_analysis/helpers.py
@@ -10,16 +10,19 @@ def get_unique_entries(resp):
     unique_entries = []
     selected_urls = {}
     selected_titles = {}
+    selected_texts = {}
 
     for hit in resp["hits"]:
         url = hit["_source"]["url"]
         title = hit["_source"]["title"]
+        text = hit["_source"]["maintext"]
 
-        if selected_urls.get(url) or selected_titles.get(title):
+        if selected_urls.get(url) or selected_titles.get(title) or selected_texts.get(text):
             continue
 
         unique_entries.append(hit)
         selected_urls[url] = 1
         selected_titles[title] = 1
+        selected_texts[text] = 1
 
     return unique_entries
